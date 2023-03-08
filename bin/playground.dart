d5b8f4df822cd7d8595b12a23b04ba00306d7dd9
Romulo Braga <romulo.braga.92@gmail.com>
2023-03-08 20:53:32 -0300

    Add Cpf challenge

--- bin/playground.dart
+++ bin/playground.dart
@@ -1,25 +1,34 @@
-import 'package:playground/playground.dart' as playground;
+import 'package:playground/cpf_validator.dart';
 
 void main(List<String> arguments) {
-  playground.calculateImc(1.7, 70);
+  // playground.calculateImc(1.7, 70);
 
-  print(playground.recursiveFibonacci(0));
-  print(playground.recursiveFibonacci(1));
-  print(playground.recursiveFibonacci(2));
-  print(playground.recursiveFibonacci(3));
-  print(playground.recursiveFibonacci(4));
-  print(playground.recursiveFibonacci(5));
-  print(playground.recursiveFibonacci(6));
-  print(playground.recursiveFibonacci(7));
+  // print(playground.recursiveFibonacci(0));
+  // print(playground.recursiveFibonacci(1));
+  // print(playground.recursiveFibonacci(2));
+  // print(playground.recursiveFibonacci(3));
+  // print(playground.recursiveFibonacci(4));
+  // print(playground.recursiveFibonacci(5));
+  // print(playground.recursiveFibonacci(6));
+  // print(playground.recursiveFibonacci(7));
 
-  print(playground.ruleOfThree(
-      group11: null, group12: 2, group21: 4, group22: 8));
-  print(playground.ruleOfThree(
-      group11: 1, group12: null, group21: 4, group22: 8));
-  print(playground.ruleOfThree(
-      group11: 1, group12: 2, group21: null, group22: 8));
-  print(playground.ruleOfThree(
-      group11: 1, group12: 2, group21: 4, group22: null));
-  print(playground.ruleOfThree(
-      group11: null, group12: null, group21: null, group22: null));
+  // print(playground.ruleOfThree(
+  //     group11: null, group12: 2, group21: 4, group22: 8));
+  // print(playground.ruleOfThree(
+  //     group11: 1, group12: null, group21: 4, group22: 8));
+  // print(playground.ruleOfThree(
+  //     group11: 1, group12: 2, group21: null, group22: 8));
+  // print(playground.ruleOfThree(
+  //     group11: 1, group12: 2, group21: 4, group22: null));
+  // print(playground.ruleOfThree(
+  //     group11: null, group12: null, group21: null, group22: null));
+  final cpf = Cpf('07675540433');
+  final cpf2 = Cpf('16425692751');
+  final cpf3 = Cpf('076.755.404-33');
+  final cpf4 = Cpf('164.256.927-51');
+
+  print(cpf.cpfValidator());
+  print(cpf2.cpfValidator());
+  print(cpf3.cpfValidator());
+  print(cpf4.cpfValidator());
 }
---
+++ lib/cpf_validator.dart
@@ -0,0 +1,44 @@
+class Cpf {
+  final String _cpf;
+
+  Cpf(this._cpf);
+
+  String get cpf => _cpf;
+
+  bool cpfValidator() {
+    final onlyNumbersCpf = cpf.replaceAll('.', '').replaceAll('-', '');
+    final firstDigitInput =
+        onlyNumbersCpf.substring(0, onlyNumbersCpf.length - 2);
+    final secondDigitInput =
+        onlyNumbersCpf.substring(0, onlyNumbersCpf.length - 1);
+
+    final validFirstDigit = digitValidationAlgorithm(firstDigitInput);
+    final validSecondDigit = digitValidationAlgorithm(secondDigitInput);
+
+    final isFirstDigitValid =
+        validFirstDigit == onlyNumbersCpf[onlyNumbersCpf.length - 2];
+    final isSecondDigitValid =
+        validSecondDigit == onlyNumbersCpf[onlyNumbersCpf.length - 1];
+
+    return (isFirstDigitValid && isSecondDigitValid);
+  }
+}
+
+String digitValidationAlgorithm(String target) {
+  var multiplyFactor = 2;
+  final targetListReversed = target.split('').reversed;
+
+  final multiplyResult = targetListReversed.map((el) {
+    final elAsNumber = int.parse(el);
+    final x = elAsNumber * multiplyFactor;
+    multiplyFactor++;
+    return x;
+  }).toList();
+
+  final sumResult = multiplyResult.reduce((acc, current) => acc + current);
+
+  final modularDivisionResult = sumResult % 11;
+
+  if (modularDivisionResult < 2) return '0';
+  return (11 - modularDivisionResult).toString();
+}
